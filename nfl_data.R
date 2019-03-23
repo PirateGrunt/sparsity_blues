43ae81e02ca8d0e30a699ec115548e3d5e20ba4a
PirateGrunt <captain@pirategrunt.com>
2019-03-23 07:35:53 -0400

    Tweak player data munge

--- nfl_data.R
+++ nfl_data.R
@@ -1,128 +1,184 @@
+library(tidyverse)
 library(httr)
-library(dplyr)
 library(jsonlite)
 library(caret)
 
-team_search_url <- "http://nflarrest.com/api/v1/team/search/"
-team_search_url <- modify_url(team_search_url, query = list(term = "s"))
-response <- GET(team_search_url)
+#================================================
+# Base URLs
+url_team_search <- "http://nflarrest.com/api/v1/team/search/"
+url_player <- "http://nflarrest.com/api/v1/team/topPlayers/"
+url_player_arrest <- 'http://NflArrest.com/api/v1/player/arrests/'
+
+#================================================
+# Get the list of team names
+url_team_search <- modify_url(url_team_search, query = list(term = "s"))
+response <- GET(url_team_search)
 the_content <- content(response, "text", encoding = "UTF-8")
 tbl_teams <- fromJSON(the_content, simplifyDataFrame =  TRUE)
 
+#================================================
+# Get players as a vector
+
 GetTeamPlayer <- function(team){
-  team_player_url <- "http://nflarrest.com/api/v1/team/topPlayers/"
-  team_player_url <- paste0(team_player_url, team)
-  response <- GET(team_player_url)
+  url_player_team <- paste0(url_player, team)
+  response <- GET(url_player_team)
   the_content <- content(response, "text", encoding = "UTF-8")
   df <- fromJSON(the_content, simplifyDataFrame =  TRUE)
-  df
+  df$Name
 }
 
-players <- lapply(tbl_teams$team_code, function(x){
-  df <- GetTeamPlayer(x)
-  players <- df$Name
-})
+players <- map(
+    tbl_teams$team_code
+  , GetTeamPlayer
+) %>%
+  flatten_chr()
 
-names(players) <- NULL
-
-players <- players %>%
-  unlist() %>%
-  unique()
+#================================================
+# Get the arrests
 
 GetPlayerArrest <- function(player){
-  player_url <- 'http://NflArrest.com/api/v1/player/arrests/'
   player <- gsub(" ", "%20", player)
-  player_url <- paste0(player_url, player)
-  response <- GET(player_url)
+  player <- gsub("'", "%27", player)
+  url_player_arrest <- paste0(url_player_arrest, player)
+  response <- GET(url_player_arrest)
   the_content <- content(response, "text", encoding = "UTF-8")
   df <- fromJSON(the_content, simplifyDataFrame =  TRUE)
   df
 }
 
-lstArrests <- lapply(players, function(x){
-  df <- GetPlayerArrest(x)
-  df
-})
+tbl_arrests <- players %>%
+  map_dfr(GetPlayerArrest)
 
-lstArrests <- lstArrests[!is.na(lstArrests)]
-tbl_arrests <- do.call(rbind, lstArrests)
+mojo <- tbl_arrests
+tbl_arrests <- mojo
+
+#==============================================
+# Munge arrests
 
 tbl_arrests <- tbl_arrests %>%
   select(
-      -Crime_category_color
+      -Team_preffered_name
+    , -Team_logo_id
+    , -Team_Conference_Division
     , -Team_hex_alt_color
     , -Team_hex_color
-    , -Team_logo_id
-    , -Team_preffered_name
-    , -Team_Conference_Division
-    , -general_category_id
-    , -resolution_category_id
-    , -legal_level_id
-    , -Year
-    , -Day
+    , -reddit_group_id
+    , -Position
+    , -Crime_category_color
+    , -YearToDate
   ) %>%
   rename(
-      ArrestDate = Date
-    , DayOfWeek = Day_of_Week
-    , ArrestID = arrest_stats_id
-    , PositionType = Position_type
-    , TeamCity = Team_city
-    , Conference = Team_Conference
-    , Division = Team_Division
-    , TeamName = Team_name
-    , CrimeCategory = Crime_category
-    , TeamAbbr = Team
-  ) %>%
-  mutate(
-      ArrestDate = as.Date(ArrestDate)
-    , ArrestID = as.numeric(ArrestID))
+      arrest_id = arrest_stats_id
+    , arrest_date = Date
+    , day_of_week = Day_of_Week
+    , position_type = Position_type
+    , team_city = Team_city
+    , conference = Team_Conference
+    , division = Team_Division
+    , team_name = Team_name
+    , crime_category = Crime_category
+    , team_abbr = Team
+    , days_since = DaysSince
+    , arrest_season_state = ArrestSeasonState
+    , days_to_last_arrest = DaysToLastArrest
+    , days_to_last_crime_arrest = DaysToLastCrimeArrest
+    , days_to_last_team_arrest = DaysToLastTeamArrest
+  )
 
-MungeVals <- function(x){
+names(tbl_arrests) <- tolower(names(tbl_arrests))
+
+munge_chars <- function(x){
   x %>%
     gsub(pattern = " / ", replacement = "_") %>%
     gsub(pattern = " ", replacement = "_")
 }
 
 one_hot_vars <- c(
-  'TeamAbbr', 'Division', 'Position'
-  , 'PositionType', 'DayOfWeek', 'Encounter'
-  , 'ArrestSeasonState', 'TeamCity', 'CrimeCategory')
+    'team_abbr'
+  , 'division'
+  , 'position_name'
+  , 'position_type'
+  , 'day_of_week'
+  , 'encounter'
+  , 'arrest_season_state'
+  , 'team_city'
+  , 'crime_category'
+)
 
-for (i in seq_along(one_hot_vars)) {
-  tbl_arrests[[one_hot_vars[i]]] <- MungeVals(tbl_arrests[[one_hot_vars[i]]])
+tbl_arrests <- tbl_arrests %>%
+  mutate_at(
+      c('arrest_id', 'season'
+        , 'year', 'month', 'day'
+        , 'days_since', 'days_to_last_arrest'
+        , 'days_to_last_crime_arrest', 'days_to_last_team_arrest')
+    , as.numeric
+  ) %>%
+  mutate_at(
+      c('arrest_date')
+    , as.Date
+  ) %>%
+  mutate_at(
+      one_hot_vars
+    , munge_chars
+  ) %>%
+  mutate(team_city = gsub("_", " ", team_city))
+
+#==================================
+# Join to cities
+load("data/cities.rda")
+
+tbl_arrests <- tbl_arrests %>%
+  left_join(tbl_cities, by = c(team_city = 'city'))
+
+missing_cities <- tbl_arrests$latitude %>%
+  is.na() %>%
+  any()
+
+if(missing_cities) {
+
+  tbl_arrests %>%
+    filter(is.na(latitude)) %>%
+    select(team_city) %>%
+    unique()
+
+
+  stop("Missing some cities!")
 }
 
+#==================================
+# Form players table
+
 tbl_players <- tbl_arrests %>%
-  select(
-      -Day_of_Week_int
-    , -DaysSince
-    , -DaysToLastArrest
-    , -DaysToLastCrimeArrest
-    , -DaysToLastTeamArrest
-    , -Month
-    , -Position_name
-    , -TeamName
-  ) %>%
-  group_by(Name) %>%
-  arrange(Name, ArrestDate) %>%
+  group_by(name) %>%
+  arrange(name, arrest_date) %>%
   slice(1) %>%
-  inner_join(summarise(group_by(tbl_arrests, Name), NumArrests = n())) %>%
+  ungroup() %>%
+  inner_join(
+    tbl_arrests %>% group_by(name) %>% summarise(num_arrests = n())
+  ) %>%
   mutate(
-      MultiArrest = (NumArrests > 1)
-    , MultiArrestNum = ifelse(MultiArrest, 1, 0)) %>%
-  select(-reddit_group_id, -NumArrests) %>%
-  ungroup()
+      multi_arrest = (num_arrests > 1)
+    , multi_arrest_num = ifelse(multi_arrest, 1, 0)
+    , multi_arrest_factor = as.factor(multi_arrest)
+  ) %>%
+  mutate_if(
+      is.character
+    , as.factor
+  )
 
-tbl_players$MultiArrestFactor <- as.factor(tbl_players$MultiArrest)
-tbl_players$PositionType <- as.factor(tbl_players$PositionType)
-tbl_players$Season <- as.factor(tbl_players$Season)
-tbl_players$ArrestSeasonState <- as.factor(tbl_players$ArrestSeasonState)
-
+tbl_arrests <- tbl_arrests %>%
+  mutate_if(
+    is.character
+    , as.factor
+  )
 
 # Why do I have more players than arrest records?
-setdiff(players, tbl_players$Name)
+setdiff(players, tbl_players$name)
 # Because I can't find record for players who have an apostrophe in their name
 
+#==========================================
+# Build one-hot encoding table.
+# Do I really need this?
 vars_formula <- paste0('~', paste0(one_hot_vars, collapse = '+')) %>%
   as.formula()
 
@@ -131,12 +187,12 @@ dummyVars <- tbl_players %>%
   predict(newdata = tbl_players) %>%
   as.data.frame()
 
-one_hot_cols <- names(dummyVars)
-ExtractVars <- function(prefix, vals){
-  grep(prefix, vals, value = TRUE)
-}
-
-one_hot_names <- sapply(one_hot_vars, ExtractVars, vals = one_hot_cols)
+# one_hot_cols <- names(dummyVars)
+# ExtractVars <- function(prefix, vals){
+#   grep(prefix, vals, value = TRUE)
+# }
+#
+# one_hot_names <- sapply(one_hot_vars, ExtractVars, vals = one_hot_cols)
 
 tbl_players_one_hot <- dplyr::bind_cols(tbl_players, dummyVars)
 
@@ -146,5 +202,4 @@ save(
   , tbl_players
   , tbl_teams
   , tbl_players_one_hot
-  , one_hot_cols
-  , one_hot_names)
+)
